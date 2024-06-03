e37a64ad0c2c5f55df955c98153aa14a947c99b1
Meikan-Sankan <meikanseikan31@gmail.com>
2024-06-03 20:21:57 +0300

    completed assignment

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,21 @@
 class Song:
-    pass
+    count = 0
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, title, artist, genre):
+        self.title = title
+        self.artist = artist
+        self.genre = genre
+
+        Song.count += 1
+
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
--- lib/testing/song_test.py
+++ lib/testing/song_test.py
@@ -1,52 +1,72 @@
 #!/usr/bin/env python3
 
+import pytest
 from song import Song
 
-Song.count = 0
-Song.genre_count = {}
-Song.artist_count = {}
-
 class TestSong:
-    '''Class "Song" in song.py'''
+    '''Test class for the Song class.'''
 
-    Song("99 Problems", "Jay Z", "Rap")
-    Song("Halo", "Beyonce", "Pop")
-    Song("Smells Like Teen Spirit", "Nirvana", "Rock")
+    def setup_method(self, method):
+        Song.count = 0
+        Song.genre_count = {}
+        Song.artist_count = {}
 
-    def test_saves_name_artist_genre(self):
-        '''instantiates with a name, artist, and genre.'''
+    def test_instantiates_with_name_artist_genre(self):
+        '''Instantiates a Song object with a title, artist, and genre.'''
         out_of_touch = Song("Out of Touch", "Hall and Oates", "Pop")
-        assert(out_of_touch.name == "Out of Touch")
-        assert(out_of_touch.artist == "Hall and Oates")
-        assert(out_of_touch.genre == "Pop")
+        assert out_of_touch.title == "Out of Touch"
+        assert out_of_touch.artist == "Hall and Oates"
+        assert out_of_touch.genre == "Pop"
 
-    def test_has_song_count(self):
-        '''counts the total number of Song objects.'''
-        assert(Song.count == 4)
+    def test_counts_total_song_objects(self):
+        '''Counts the total number of Song objects created.'''
+        Song("99 Problems", "Jay Z", "Rap")
+        Song("Halo", "Beyonce", "Pop")
+        Song("Smells Like Teen Spirit", "Nirvana", "Rock")
+        assert Song.count == 3
         Song("Sara Smile", "Hall and Oates", "Pop")
-        assert(Song.count == 5)
+        assert Song.count == 4
 
-    def test_has_genres(self):
-        '''keeps track of all Song genres.'''
-        assert("Rap" in Song.genres)
-        assert("Pop" in Song.genres)
-        assert("Rock" in Song.genres)
+    def test_tracks_all_song_genres(self):
+        '''Tracks all the different Song genres.'''
+        Song("99 Problems", "Jay Z", "Rap")
+        Song("Halo", "Beyonce", "Pop")
+        Song("Smells Like Teen Spirit", "Nirvana", "Rock")
+        assert "Rap" in Song.genre_count
+        assert "Pop" in Song.genre_count
+        assert "Rock" in Song.genre_count
 
-    def test_has_artists(self):
-        '''keeps track of all Song artists.'''
-        assert("Jay Z" in Song.artists)
-        assert("Beyonce" in Song.artists)
-        assert("Hall and Oates" in Song.artists)
-        
-    def test_has_genre_count(self):
-        '''keeps count of Songs for each genre.'''
-        assert(Song.genre_count["Rap"] == 1)
-        assert(Song.genre_count["Pop"] == 3)
-        assert(Song.genre_count["Rock"] == 1)
+    def test_tracks_all_song_artists(self):
+        '''Tracks all the different Song artists.'''
+        Song("99 Problems", "Jay Z", "Rap")
+        Song("Halo", "Beyonce", "Pop")
+        Song("Smells Like Teen Spirit", "Nirvana", "Rock")
+        assert "Jay Z" in Song.artist_count
+        assert "Beyonce" in Song.artist_count
+        assert "Nirvana" in Song.artist_count
+        Song("Sara Smile", "Hall and Oates", "Pop")
+        assert "Hall and Oates" in Song.artist_count
 
-    def test_has_artist_count(self):
-        '''keeps count of Songs for each artist.'''
-        assert(Song.artist_count["Jay Z"] == 1)
-        assert(Song.artist_count["Beyonce"] == 1)
-        assert(Song.artist_count["Nirvana"] == 1)
-        assert(Song.artist_count["Hall and Oates"] == 2)
+    def test_tracks_song_count_for_each_genre(self):
+        '''Tracks the count of songs for each genre.'''
+        Song("99 Problems", "Jay Z", "Rap")
+        Song("Halo", "Beyonce", "Pop")
+        Song("Smells Like Teen Spirit", "Nirvana", "Rock")
+        Song("Sara Smile", "Hall and Oates", "Pop")
+        assert Song.genre_count["Rap"] == 1
+        assert Song.genre_count["Pop"] == 2
+        assert Song.genre_count["Rock"] == 1
+
+    def test_tracks_song_count_for_each_artist(self):
+        '''Tracks the count of songs for each artist.'''
+        Song("99 Problems", "Jay Z", "Rap")
+        Song("Halo", "Beyonce", "Pop")
+        Song("Smells Like Teen Spirit", "Nirvana", "Rock")
+        Song("Sara Smile", "Hall and Oates", "Pop")
+        assert Song.artist_count["Jay Z"] == 1
+        assert Song.artist_count["Beyonce"] == 1
+        assert Song.artist_count["Nirvana"] == 1
+        assert Song.artist_count["Hall and Oates"] == 1
+
+if __name__ == "__main__":
+    pytest.main()
